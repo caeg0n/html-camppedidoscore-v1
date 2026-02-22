99be919a610905146d6240072f283897c96e7b6d
caeg0n <caegon2005@hotmail.com>
2026-02-22 20:24:58 -0300

    Deploy cardapio web

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,2 +1,12 @@
-window.CAMPP_FIREBASE_CONFIG = null;
+﻿window.CAMPP_FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
 window.CAMPP_APP_KEY = "html-camppedidoscore-v1";
+
